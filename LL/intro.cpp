211210c442b69a3853141ebe7c8869af8db477c4
Anshul Kumar <anshul2004ak@gmail.com>
2025-07-29 20:08:16 +0530

    debug kra sab kuch

---
+++ LL/intro.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int main(){
+    vector<int> arr = {2, 5, 8, 7};
+    Node* y = new Node(arr[0]);
+    cout << y->data;
+}
